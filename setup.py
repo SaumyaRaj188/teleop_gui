b1e69184a1909c95a3211842c66129584ebb3561
Saumya Raj Singh <saumyaraj188@gmail.com>
2023-05-20 03:49:12 +0530

    Updated maintainer

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='ARES',
-    maintainer_email='TODO',
+    maintainer_email='myemail@gmail.com',
     description='TODO: Package description',
     license='TODO: License declaration',
     entry_points= {
